29d6651ffb89a4e93a39e75e632737dbca4f9d9d
Pavel73 <pasha.nenahov@gmail.com>
2024-11-12 02:45:49 +0100

    created: product page product class

--- My-Grocery-Store/Program.cs
+++ My-Grocery-Store/Program.cs
@@ -1,3 +1,5 @@
+using AddProductPage;
+
 namespace My_Grocery_Store
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace My_Grocery_Store
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new AddProduct());
         }
     }
 }
---
+++ My-Grocery-Store/RandCustNum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AddProductPage
+{
+    public static class RandCustNum
+    {
+        public static string generate() 
+        {
+            int randNum = new Random().Next(0,10000);
+            string id = randNum.ToString("D4");
+
+            return id;
+        }
+    }
+}
