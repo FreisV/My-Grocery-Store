fe85a176a0836479194cb8658b81bdaa48ee6f50
Alejandro <alejandrojimenez2718@gmail.com>
2024-11-13 12:48:53 +0100

    Add the MainMenu into the branch main-menu with the other menus "AddProduct" and "ShoppingMenu"

---
+++ My-Grocery-Store/MainMenu.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace My_Grocery_Store
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(65, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(341, 46);
+            label1.TabIndex = 0;
+            label1.Text = "Nefor Grocery Store";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Lime;
+            button1.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(117, 86);
+            button1.Name = "button1";
+            button1.Size = new Size(236, 57);
+            button1.TabIndex = 1;
+            button1.Text = "Add Products";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Aquamarine;
+            button2.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(117, 172);
+            button2.Name = "button2";
+            button2.Size = new Size(236, 103);
+            button2.TabIndex = 2;
+            button2.Text = "Shopping Menu";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.IndianRed;
+            button3.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(149, 304);
+            button3.Name = "button3";
+            button3.Size = new Size(164, 49);
+            button3.TabIndex = 3;
+            button3.Text = "Exit";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(12, 393);
+            label2.Name = "label2";
+            label2.Size = new Size(182, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Programmed by Nefors";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(245, 393);
+            label3.Name = "label3";
+            label3.Size = new Size(203, 20);
+            label3.TabIndex = 5;
+            label3.Text = "® All rights reserved 2024";
+            // 
+            // MainMenu
+            // 
+            AutoScaleDimensions = new SizeF(8F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(460, 422);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Font = new Font("Microsoft Sans Serif", 8.25F);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Name = "MainMenu";
+            Text = "MainMenu";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label2;
+        private Label label3;
+    }
+}
